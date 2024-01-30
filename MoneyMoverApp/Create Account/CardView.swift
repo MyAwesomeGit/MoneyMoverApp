645aee9b1f9611387316441d28b7cdc937578489
Vasily Shuklin <b88bb@yandex.ru>
2024-01-30 20:38:11 +0500

    Add card information to CardView

--- MoneyMoverApp/Create Account/CardView.swift
+++ MoneyMoverApp/Create Account/CardView.swift
@@ -4,7 +4,7 @@ import SwiftUI
 struct CardView: View {
     var body: some View {
         ZStack {
-            HStack(spacing: -166) {
+            HStack(spacing: -116) {
                 Image("left-card")
                     .renderingMode(.template)
                     .foregroundColor(.black)
@@ -19,35 +19,53 @@ struct CardView: View {
                     Image("checkmark")
                     VStack(alignment: .leading, spacing:  -6) {
                         Text("1 000 000 ₽")
-                            .font(.title)
+                            .font(.title2)
                             .fontWeight(.bold)
                         Text("Available balance")
                             .font(.subheadline)
-                        Spacer()
-                        Image("visa-logo")
                     }
-                    .padding(.horizontal, 20)
+                    Spacer()
+                    Image("visa-logo")
+                }
+                .padding(.horizontal, 20)
+                HStack {
                     HStack {
-                        HStack {
-                            Text("****")
-                            Text("****")
-                            Text("****")
-                            Text("0708")
-                        }
-                        .font(.system(size: 8))
-                        Spacer()
+                        Text("****")
+                        Text("****")
+                        Text("****")
+                        Text("0708")
                     }
-                    .padding(.top, 15)
-                    .padding(.horizontal, 20)
+                    .font(.system(size: 12))
                     Spacer()
                 }
-                .frame(width: 280, height: 160)
-                .padding(10)
-                .foregroundColor(.white)
+                .padding(.top, 15)
+                .padding(.horizontal, 20)
+                Spacer()
+                
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("CARD HOLDER")
+                        Text("Cardholder Name")
+                    }
+                    .foregroundColor(Color.white)
+                    .font(.system(size: 12))
+                    Spacer()
+                    
+                    VStack(alignment: .leading) {
+                        Text("EXPIRES")
+                        Text("01/28")
+                    }
+                    .foregroundColor(Color.white)
+                    .font(.system(size: 10))
+                }
             }
+            .frame(width: 280, height: 160)
+            .padding(10)
+            .foregroundColor(.white)
         }
     }
 }
+
 
 struct CardView_Previews: PreviewProvider {
     static var previews: some View {
