2c13c2b3adfe922077bab16b23f901d35a9ed981
Vasily Shuklin <b88bb@yandex.ru>
2024-01-30 20:13:26 +0500

    Add available balance to CardView

---
+++ MoneyMoverApp/Create Account/CardView.swift
@@ -0,0 +1,44 @@
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        ZStack {
+            HStack(spacing: -166) {
+                Image("left-card")
+                    .renderingMode(.template)
+                    .foregroundColor(.black)
+                Image("right-card")
+                    .renderingMode(.template)
+                    .foregroundColor(.black)
+                    .opacity(0.94)
+            }
+            
+            VStack {
+                HStack {
+                    Image("checkmark")
+                    VStack(alignment: .leading, spacing:  -6) {
+                        Text("1 000 000 ₽")
+                            .font(.title)
+                            .fontWeight(.bold)
+                        Text("Available balance")
+                            .font(.subheadline)
+                        Spacer()
+                        Image("visa-logo")
+                    }
+                    .padding(.horizontal, 20)
+                }
+                .frame(width: 280, height: 160)
+                .padding(10)
+                .foregroundColor(.white)
+            }
+        }
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+            .previewLayout(.fixed(width: 300, height: 180))
+    }
+}
