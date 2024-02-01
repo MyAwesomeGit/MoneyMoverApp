f72be66f7c6ab19b7ef04729f6ae13de41e725a3
Vasily Shuklin <b88bb@yandex.ru>
2024-02-01 19:50:18 +0500

    Add AccountFormView

---
+++ MoneyMoverApp/View/AccountFormView/AccountFormView.swift
@@ -0,0 +1,32 @@
+
+import SwiftUI
+
+struct AccountFormView: View {
+    private var type = "creditcard"
+    
+    init() {
+        UITableView.appearance().backgroundColor = UIColor.white
+        UITableView.appearance().tableFooterView = UIView(frame: CGRect(x: 0, y: 0, width: 0, height: Double.leastNonzeroMagnitude))
+        UITableView.appearance().separatorStyle = .none
+    }
+    
+    var body: some View {
+        List {
+            Section {
+
+            }
+            .listRowBackground(Color.white)
+            .listRowInsets(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 0))
+        }
+        .listStyle(GroupedListStyle())
+        .background(Color.white)
+    }
+}
+
+
+struct AccountFormView_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountFormView()
+            .previewLayout(.fixed(width: 600, height: 200))
+    }
+}
