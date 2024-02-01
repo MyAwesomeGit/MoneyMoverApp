37c8622a0de89b9c93bbb8f2634e559b95cefdf0
Vasily Shuklin <b88bb@yandex.ru>
2024-02-01 19:45:45 +0500

    Add ColorButtonMenu

---
+++ MoneyMoverApp/View/Home/ColorButtonMenu.swift
@@ -0,0 +1,42 @@
+
+import SwiftUI
+
+struct ColorButtonMenu: View {
+    private var selectedSegment: Int = 0
+    private var selectedColor: Color = Color(red: -0.004, green: 0.239, blue: 0.43)
+    private var colors: [Color] = [
+        .blue,
+        .black,
+        .red,
+        .green,
+        .purple,
+        .gray
+    ]
+    
+    var body: some View {
+        HStack {
+            ForEach(0 ..< colors.count) { index in
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    ZStack {
+                        ColorView(color: self.colors[index])
+                        Image("checkmark-selector")
+                            .resizable()
+                            .renderingMode(.template)
+                            .opacity(self.selectedSegment == index ? 1 : 0)
+                            .frame(width: 12, height: 10)
+                            .foregroundColor(.white)
+                    }
+                }
+                .buttonStyle(PlainButtonStyle())
+            }
+        }
+    }
+}
+
+
+struct ColorButtonMenu_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorButtonMenu()
+            .previewLayout(.fixed(width: 300, height: 50))
+    }
+}
