627e9586cc2e3661a5761b945ecadf288b64adbd
Vasily Shuklin <b88bb@yandex.ru>
2024-02-02 18:00:03 +0500

    Add AccountListView

---
+++ MoneyMoverApp/View/Create Account View/AccountListView.swift
@@ -0,0 +1,39 @@
+
+import SwiftUI
+
+struct AccountListView: View {
+    init() {
+            UITableView.appearance().separatorStyle = .none
+            UITableViewCell.appearance().backgroundColor = .clear
+            UITableView.appearance().backgroundColor = .clear
+        }
+    
+    var body: some View {
+        NavigationView {
+            VStack(alignment: .leading) {
+                List {
+                    ForEach(0..<4) { account in
+                        HStack {
+                            Spacer()
+                            
+                            NavigationLink(destination: AccountHomeView()) {
+                                
+                            }.buttonStyle(PlainButtonStyle())
+                            
+                            Spacer()
+                        }
+                        .padding()
+                    }
+                }
+            }
+            .navigationBarTitle("Accounts")
+        }
+    }
+}
+
+
+struct AccountListView_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountListView()
+    }
+}
