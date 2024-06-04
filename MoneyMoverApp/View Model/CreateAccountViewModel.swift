c4c8af97080e67ed35107d2bb53f1203231ae6b5
Vasily Shuklin <b88bb@yandex.ru>
2024-06-04 20:26:30 +0500

    Add CreateAccountViewModel

---
+++ MoneyMoverApp/View Model/CreateAccountViewModel.swift
@@ -0,0 +1,33 @@
+
+import SwiftUI
+import Combine
+import CoreData
+
+enum AccountType: String {
+    case debit = "Debit Card"
+    case creditcard = "Credit Card"
+}
+
+
+enum CardType: String {
+    case mir
+    case visa
+    case mastercard
+}
+
+
+class CreateAccountViewModel: ObservableObject {
+    
+    func createAccount() {
+    }
+    
+    func hasAccounts() {
+    }
+    
+    func createRandomBalance() {
+    }
+    
+    func clear() {
+    }
+
+}
