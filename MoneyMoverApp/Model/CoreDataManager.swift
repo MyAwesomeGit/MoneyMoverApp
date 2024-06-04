4af1b3b93b8ae9279d2085f4d6bd10c50398a486
Vasily Shuklin <b88bb@yandex.ru>
2024-06-04 20:19:09 +0500

    Add CoreDataManager singleton

---
+++ MoneyMoverApp/Model/CoreDataManager.swift
@@ -0,0 +1,29 @@
+
+import Foundation
+import CoreData
+
+class CoreDataManager: ObservableObject {
+    static var shared = CoreDataManager()
+    
+    var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "MoneyMoverApp")
+        container.loadPersistentStores { (storeDescription, error) in
+            guard error == nil
+            else {
+                print(error?.localizedDescription as Any)
+                return
+            }
+        }
+        return container
+    }()
+    
+    private init() {
+        persistentContainer.viewContext.automaticallyMergesChangesFromParent = true
+    }
+    
+    public var context: NSManagedObjectContext {
+        get {
+            return self.persistentContainer.viewContext
+        }
+    }
+}
--- MoneyMoverApp/MoneyMoverAppApp.swift
+++ MoneyMoverApp/MoneyMoverAppApp.swift
@@ -3,6 +3,9 @@ import SwiftUI
 
 @main
 struct MoneyMoverAppApp: App {
+    
+    let manager = CoreDataManager.shared
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
