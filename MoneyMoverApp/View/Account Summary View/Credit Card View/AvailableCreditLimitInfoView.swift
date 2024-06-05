f56aa8320270e774c3a8f23e40cee60acdb6ed76
Vasily Shuklin <b88bb@yandex.ru>
2024-06-05 20:30:49 +0500

    Update AvailableCreditLimitInfo background color

--- MoneyMoverApp/View/Account Summary View/Credit Card View/AvailableCreditLimitInfoView.swift
+++ MoneyMoverApp/View/Account Summary View/Credit Card View/AvailableCreditLimitInfoView.swift
@@ -1,30 +1,34 @@
-
 import SwiftUI
+
 
 struct AvailableCreditLimitInfo: View {
     var body: some View {
-        HStack {
-            VStack(alignment: .leading, spacing: 0) {
-                Text("Credit utilized")
-                    .font(.system(size: 18))
-                    .foregroundColor(.black)
-                Text("100 000 ₽")
-                    .font(.system(size: 24))
-                    .fontWeight(.bold)
-                    .foregroundColor((Color(red: -0.004, green: 0.239, blue: 0.43)))
+        ZStack {
+            Color(.systemGray)
+                .edgesIgnoringSafeArea(.all)
+            HStack {
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Credit utilized")
+                        .font(.system(size: 18))
+                        .foregroundColor(Color(.black))
+                    Text("100 000 ₽")
+                        .font(.system(size: 24))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color(.black))
+                }
+                Spacer()
+                VStack(alignment: .trailing, spacing: 0) {
+                    Text("Available credit limit")
+                        .font(.system(size: 18))
+                        .foregroundColor(Color(.black))
+                    Text("500 000 ₽")
+                        .font(.system(size: 24))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color(.black))
+                }
             }
-            Spacer()
-            VStack(alignment: .trailing, spacing: 0) {
-                Text("Available credit limit")
-                    .font(.system(size: 18))
-                    .foregroundColor(.black)
-                Text("500 000 ₽")
-                    .font(.system(size: 24))
-                    .fontWeight(.bold)
-                    .foregroundColor((Color(red: -0.004, green: 0.239, blue: 0.43)))
-            }
+            .padding(.top, 10)
         }
-        .padding(.top, 10)
     }
 }
 
