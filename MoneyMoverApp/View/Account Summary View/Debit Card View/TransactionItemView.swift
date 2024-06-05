70639173ae3e43cc40f48b9fec06e969ead113ce
Vasily Shuklin <b88bb@yandex.ru>
2024-06-05 20:42:15 +0500

    Update TransactionItemView UI

--- MoneyMoverApp/View/Account Summary View/Debit Card View/TransactionItemView.swift
+++ MoneyMoverApp/View/Account Summary View/Debit Card View/TransactionItemView.swift
@@ -3,24 +3,30 @@ import SwiftUI
 
 struct TransactionItemView: View {
     var body: some View {
-        HStack {
-            Image("icon-payment-from")
-            VStack(alignment: .leading) {
-                Text("Payment")
-                    .font(.system(size: 24))
-                    .foregroundColor(Color(red: -0.004, green: 0.239, blue: 0.43))
-                Text("Payment from Carl")
-                    .font(.system(size: 18))
-            }
+        ZStack {
+            Color(.systemGray)
+                .edgesIgnoringSafeArea(.all)
             
-            VStack(alignment: .trailing) {
-                Text("+ 5 000 ₽")
-                    .font(.system(size: 24))
-                    .fontWeight(.bold)
-                    .foregroundColor(Color(red: -0.004, green: 0.239, blue: 0.43))
-                Text("Jan 31")
-                    .font(.system(size: 18))
+            HStack {
+                Image("icon-payment-from")
+                VStack(alignment: .leading) {
+                    Text("Payment")
+                        .font(.system(size: 24))
+                        .foregroundColor(Color(red: -0.004, green: 0.239, blue: 0.43))
+                    Text("Payment from Carl")
+                        .font(.system(size: 18))
+                }
+                
+                VStack(alignment: .trailing) {
+                    Text("+ 5 000 ₽")
+                        .font(.system(size: 24))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color(red: -0.004, green: 0.239, blue: 0.43))
+                    Text("Jan 31")
+                        .font(.system(size: 18))
+                }
             }
+
         }
     }
 }
