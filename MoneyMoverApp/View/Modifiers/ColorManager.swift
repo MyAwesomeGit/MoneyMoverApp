d579c445345eb45bee7b6fa1d0796d6e7d996d0e
Vasily Shuklin <b88bb@yandex.ru>
2024-06-05 20:43:39 +0500

    Add ColorManager

---
+++ MoneyMoverApp/View/Modifiers/ColorManager.swift
@@ -0,0 +1,8 @@
+
+import SwiftUI
+
+let mainBackgroundColor = Color(red: 0.26, green: 0.28, blue: 0.33)
+
+let mainFontColor = Color(red: 2.2, green: 2.2, blue: 2.1)
+
+let secondaryFontColor = Color(red: 0.93, green: 1.15, blue: 1.52)
