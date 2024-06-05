07a86df50de70d14529fe57d482eede2beaa722e
Vasily Shuklin <b88bb@yandex.ru>
2024-06-05 20:33:31 +0500

    Update PaymentInfoView UI

--- MoneyMoverApp/View/Account Summary View/Credit Card View/PaymentInfoView.swift
+++ MoneyMoverApp/View/Account Summary View/Credit Card View/PaymentInfoView.swift
@@ -1,43 +1,49 @@
-
 import SwiftUI
 
 struct PaymentInfo: View {
     var body: some View {
-        List {
-            HStack {
-                Text("Last bill amount")
-                    .font(.system(size: 16))
-                    .foregroundColor(.black)
-                Spacer()
-                Text("200 000 ₽")
-                    .font(.system(size: 16))
-                    .foregroundColor(.black)
+        ZStack {
+            Color(.systemGray)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                HStack {
+                    Text("Last bill amount")
+                        .font(.system(size: 20))
+                        .foregroundColor(.black)
+                    Spacer()
+                    Text("200 000 ₽")
+                        .font(.system(size: 20))
+                        .foregroundColor(.black)
+                }
+                .listRowBackground(Color.white)
+                
+                Divider()
+                
+                HStack {
+                    Text("Payment due date")
+                        .font(.system(size: 20))
+                        .foregroundColor(.black)
+                    Spacer()
+                    Text("Feb 20, 2024")
+                        .font(.system(size: 20))
+                        .foregroundColor(.black)
+                }
+                .listRowBackground(Color.white)
+                Divider()
+                
+                HStack {
+                    Text("Min. amount due date")
+                        .font(.system(size: 20))
+                        .foregroundColor(.black)
+                    Spacer()
+                    Text("50 000 ₽")
+                        .font(.system(size: 20))
+                        .foregroundColor(.black)
+                }
             }
-            .listRowBackground(Color.white)
-            
-            HStack {
-                Text("Payment due date")
-                    .font(.system(size: 16))
-                    .foregroundColor(.black)
-                Spacer()
-                Text("Feb 20, 2024")
-                    .font(.system(size: 16))
-                    .foregroundColor(.black)
-            }
-            .listRowBackground(Color.white)
-            
-            HStack {
-                Text("Min. amount due date")
-                    .font(.system(size: 16))
-                    .foregroundColor(.black)
-                Spacer()
-                Text("50 000 ₽")
-                    .font(.system(size: 16))
-                    .foregroundColor(.black)
-            }
-            .listRowBackground(Color.white)
+            .padding(.all)
+            .frame(width: 410, height: 200)
         }
-        .frame(width: 410, height: 200)
     }
 }
 
