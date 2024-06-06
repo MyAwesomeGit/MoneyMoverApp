851da31178a8080e08b1a5a7672542ab14c00af4
Vasily Shuklin <b88bb@yandex.ru>
2024-06-06 20:29:39 +0500

    Update TransactionsView UI

--- MoneyMoverApp/View/Account Summary View/Debit Card View/TransactionsView.swift
+++ MoneyMoverApp/View/Account Summary View/Debit Card View/TransactionsView.swift
@@ -4,20 +4,11 @@ import SwiftUI
 struct TransactionsView: View {
     var body: some View {
         ZStack {
-            Color(.systemGray)
+            Color(mainBackgroundColor)
                 .edgesIgnoringSafeArea(.all)
-            
-            VStack(spacing: 0) {
-                HStack {
                     Text("Transactions")
                         .font(.system(size: 24))
-                        .padding(.bottom, 2)
-                        .foregroundColor(Color(red: -0.004, green: 0.239, blue: 0.43))
-                    Spacer()
-                }
-                Divider()
-                    .padding(.bottom, 10)
-            }
+                        .foregroundColor(Color(mainFontColor))
         }
     }
 }
