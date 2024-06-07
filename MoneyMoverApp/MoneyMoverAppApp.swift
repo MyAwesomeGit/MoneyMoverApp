9e511e678e5eeb6d882ebfa1eedd620a1466ab92
Vasily Shuklin <b88bb@yandex.ru>
2024-06-07 18:51:01 +0500

    Update MoneyMoverApp

--- MoneyMoverApp/MoneyMoverAppApp.swift
+++ MoneyMoverApp/MoneyMoverAppApp.swift
@@ -4,6 +4,8 @@ import SwiftUI
 @main
 struct MoneyMoverAppApp: App {
     
+    @StateObject private var model = CreateAccountViewModel()
+    
     let manager = CoreDataManager.shared
     
     var body: some Scene {
