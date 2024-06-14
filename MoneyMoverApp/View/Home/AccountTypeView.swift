2cc6a7075f4e5e7d3b23c0441d18a70caab02c02
Vasily Shuklin <b88bb@yandex.ru>
2024-06-14 19:00:18 +0500

    Update AccountTypeView with model

--- MoneyMoverApp/View/Home/AccountTypeView.swift
+++ MoneyMoverApp/View/Home/AccountTypeView.swift
@@ -2,28 +2,36 @@
 import SwiftUI
 
 struct AccountTypeView: View {
+    
+    @StateObject private var model = CreateAccountViewModel()
+    
     var body: some View {
-        HStack(spacing: 15) {
-            ForEach(0 ..< 2) { index in
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-                    HStack {
-                        ZStack {
-                            Circle()
-                                .fill(Color(red: -0.004, green: 0.239, blue: 0.43))
-                                .frame(width: 18, height: 18)
-                            Image("checkmark-selector")
-                                .resizable()
-                                .renderingMode(.template)
-                                .frame(width: 10, height: 8)
-                                .foregroundColor(.white)
+        ZStack {
+            Color(mainBackgroundColor)
+                .edgesIgnoringSafeArea(.all)
+            
+            HStack(spacing: 15) {
+                ForEach(0 ..< model.accountTypes.count) { index in
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                        HStack {
+                            ZStack {
+                                Circle()
+                                    .fill(Color(red: -0.004, green: 0.239, blue: 0.43))
+                                    .frame(width: 18, height: 18)
+                                Image("checkmark-selector")
+                                    .resizable()
+                                    .renderingMode(.template)
+                                    .frame(width: 10, height: 8)
+                                    .foregroundColor(mainFontColor)
+                            }
+                            Text(self.model.accountTypes[index])
+                                .font(.system(size: 18))
+                                .foregroundColor(mainFontColor)
                         }
-                        Text("Account types")
-                            .font(.system(size: 18))
-                            .foregroundColor(.gray)
+                        .buttonStyle(PlainButtonStyle())
                     }
-                    .buttonStyle(PlainButtonStyle())
+                    Spacer()
                 }
-                Spacer()
             }
         }
     }
