c2e6652747253183821da72e09ee6d52c11cab4b
Vasily Shuklin <b88bb@yandex.ru>
2024-06-28 18:21:26 +0500

    Update FirstNameView

--- MoneyMoverApp/View/Create Account View/Account Form View/FirstNameView.swift
+++ MoneyMoverApp/View/Create Account View/Account Form View/FirstNameView.swift
@@ -5,19 +5,32 @@ struct FirstNameView: View {
     @State var firstName = "First Name"
     
     var body: some View {
-        VStack(alignment: .center, spacing: 10) {
-            HStack(alignment: .center, spacing: 0) {
-                Text("FIRST NAME")
-                    .font(.system(size: 18))
-                .foregroundColor(.blue)
+        ZStack {
+            Color(mainBackgroundColor)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack(alignment: .center, spacing: 10) {
+                HStack(alignment: .center, spacing: 0) {
+                    Text("FIRST NAME")
+                        .font(.system(size: 18))
+                    .foregroundColor(secondaryFontColor)
 
-                TextField("First Name", text: $firstName)
-                    .multilineTextAlignment(.trailing)
-                    .font(.system(size: 18))
-                    .padding(.trailing, 10)
-            }.padding(.horizontal, 0)
+                    TextField("First Name", text: $firstName)
+                        .multilineTextAlignment(.trailing)
+                        .font(.system(size: 18))
+                        .foregroundColor(mainFontColor)
+                        .padding(.trailing, 10)
+                }.padding(.horizontal, 0)
+            }
+            .frame(height: 40)
+            .padding(.top, 10)
         }
-        .frame(height: 40)
-        .padding(.top, 10)
+    }
+}
+
+
+struct FirstNameView_Previews: PreviewProvider {
+    static var previews: some View {
+        FirstNameView()
     }
 }
