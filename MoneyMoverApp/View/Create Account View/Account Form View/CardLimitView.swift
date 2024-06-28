fc814499e6f35abc13a6270f377adede55a837c0
Vasily Shuklin <b88bb@yandex.ru>
2024-06-28 18:24:28 +0500

    Update CardLimitView

--- MoneyMoverApp/View/Create Account View/Account Form View/CardLimitView.swift
+++ MoneyMoverApp/View/Create Account View/Account Form View/CardLimitView.swift
@@ -5,37 +5,53 @@ struct CardLimitView: View {
     @State var limit = "Credit Limit"
     
     var body: some View {
-        VStack(alignment: .center, spacing: 10) {
-            HStack(alignment: .center, spacing: 0) {
-                
-                Text("CREDIT LIMIT")
-                    .font(.system(size: 18))
-                    .foregroundColor(.blue)
+        ZStack {
+            Color(mainBackgroundColor)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack(alignment: .center, spacing: 10) {
+                HStack(alignment: .center, spacing: 0) {
+                    
+                    Text("CREDIT LIMIT")
+                        .font(.system(size: 18))
+                        .foregroundColor(secondaryFontColor)
 
-                TextField("Credit Limit", text: $limit, onEditingChanged: { _ in
+                    TextField("Credit Limit", text: $limit, onEditingChanged: { _ in
 
-                }, onCommit: {
+                    }, onCommit: {
 
-                }).multilineTextAlignment(.trailing)
-                    .font(.system(size: 18))
-                    .padding(.trailing, limit == "" ? -5 : 10)
-                
-                Button(action: {
-                    limit = ""
-                }) {
-                    Image(systemName: "xmark.circle.fill")
-                        .opacity(limit == "" ? 0 : 1)
-                        .offset(x: limit == "" ? 35 : 0)
-                        .padding(.trailing, limit == "" ? 0 : 5)
+                    }).multilineTextAlignment(.trailing)
+                        .font(.system(size: 18))
+                        .foregroundColor(mainFontColor)
+
+                        .padding(.trailing, limit == "" ? -5 : 10)
+                    
+                    Button(action: {
+                        limit = ""
+                    }) {
+                        Image(systemName: "xmark.circle.fill")
+                            .opacity(limit == "" ? 0 : 1)
+                            .offset(x: limit == "" ? 35 : 0)
+                            .foregroundColor(secondaryFontColor)
+                            .padding(.trailing, limit == "" ? 0 : 5)
+                    }
                 }
-            }
-            .padding(.horizontal, 0)
+                .padding(.horizontal, 0)
 
-            Divider()
+                Divider()
+            }
+            .frame(height: 40)
+            .animation(.easeInOut, value: limit)
+            .padding(.top, 10)
+
+
         }
-        .frame(height: 40)
-        .animation(.easeInOut, value: limit)
-        .padding(.top, 10)
     }
 }
 
+
+struct CardLimitView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardLimitView()
+    }
+}
