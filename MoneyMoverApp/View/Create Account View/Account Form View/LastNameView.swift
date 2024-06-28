f9dbd60bf5024c195100f403468574429a663c2e
Vasily Shuklin <b88bb@yandex.ru>
2024-06-28 18:22:49 +0500

    Update LastNameView

--- MoneyMoverApp/View/Create Account View/Account Form View/LastNameView.swift
+++ MoneyMoverApp/View/Create Account View/Account Form View/LastNameView.swift
@@ -5,19 +5,32 @@ struct LastNameView: View {
     @State var lastName = "Last Name"
     
     var body: some View {
-        VStack(alignment: .center, spacing: 10) {
-            HStack(alignment: .center, spacing: 0) {
-                Text("LAST NAME")
-                    .font(.system(size: 18))
-                .foregroundColor(.blue)
+        ZStack {
+            Color(mainBackgroundColor)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack(alignment: .center, spacing: 10) {
+                HStack(alignment: .center, spacing: 0) {
+                    Text("LAST NAME")
+                        .font(.system(size: 18))
+                    .foregroundColor(secondaryFontColor)
 
-                TextField("Last Name", text: self.$lastName)
-                    .multilineTextAlignment(.trailing)
-                    .font(.system(size: 18))
-                    .padding(.trailing, 10)
-            }.padding(.horizontal, 0)
+                    TextField("Last Name", text: self.$lastName)
+                        .multilineTextAlignment(.trailing)
+                        .font(.system(size: 18))
+                        .foregroundColor(mainFontColor)
+                        .padding(.trailing, 10)
+                }.padding(.horizontal, 0)
+            }
+            .frame(height: 40)
+            .padding(.top, 10)
         }
-        .frame(height: 40)
-        .padding(.top, 10)
+    }
+}
+
+
+struct LastNameView_Previews: PreviewProvider {
+    static var previews: some View {
+        LastNameView()
     }
 }
