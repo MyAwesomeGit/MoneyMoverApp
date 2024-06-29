83b0937671547bd0b1c1fbf61650b763249e7681
Vasily Shuklin <b88bb@yandex.ru>
2024-06-29 12:51:56 +0500

    Add animation to LoadingView

--- MoneyMoverApp/View/Home/LoadingView.swift
+++ MoneyMoverApp/View/Home/LoadingView.swift
@@ -2,19 +2,35 @@
 import SwiftUI
 
 struct LoadingView: View {
+    @State private var loadingText = ""
+    @State private var index = 0.0
+    
     var body: some View {
         VStack {
             ProgressView()
-               .progressViewStyle(CircularProgressViewStyle())
-               .scaleEffect(2)
+                .progressViewStyle(CircularProgressViewStyle())
+                .scaleEffect(2)
             
-            Text("Loading...")
-               .font(.largeTitle)
-               .foregroundColor(.white)
+            Text(loadingText)
+                .font(.largeTitle)
+                .foregroundColor(.white)
         }
-       .frame(maxWidth:.infinity, maxHeight:.infinity)
-       .background(mainBackgroundColor.opacity(1.0))
-       .edgesIgnoringSafeArea(.all)
+        .frame(maxWidth:.infinity, maxHeight:.infinity)
+        .background(mainBackgroundColor.opacity(1.0))
+        .edgesIgnoringSafeArea(.all)
+        .onAppear {
+            animateText()
+        }
+    }
+    
+    func animateText() {
+        let textToAnimate = "MoneyMover"
+        for letter in textToAnimate {
+            Timer.scheduledTimer(withTimeInterval: 0.15 * Double(index), repeats: false) { timer in
+                self.loadingText.append(letter)
+            }
+            index += 1
+        }
     }
 }
 
